bb9ee984a8f009f2e0ffb25afd3f6f816f35a81b
Tom Coleman <tom@chromatic.com>
2022-01-12 15:58:07 +1100

    added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  },
+  "webpackFinal": (config) => {
+      const path = require('path');
+      // add monorepo root as a valid directory to import modules from
+      config.resolve.plugins.forEach((p) => {
+        if (Array.isArray(p.appSrcs)) {
+          p.appSrcs.push(path.join(__dirname, '..', '..', '..', 'storybook'));
+        }
+      });
+      return config;
+    }
+    
+}
